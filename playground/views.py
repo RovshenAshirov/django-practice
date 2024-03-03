8e85fd18dbd0a803136584e0cbabe658d8cc6144
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-03-03 20:21:45 +0500

    Using Templates

--- playground/views.py
+++ playground/views.py
@@ -10,4 +10,5 @@ def say_hello(request):
     # Pull data from db
     # Transform
     # Send email
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, 'hello.html', {'name': 'Rovshen'})
