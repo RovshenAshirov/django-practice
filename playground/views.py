784b4eae8be5ada327af57fd6f94d375c067c828
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-03-03 19:12:00 +0500

    Writing Views

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+# request -> response
+# request handler
+# action
+def say_hello(request):
+    # Pull data from db
+    # Transform
+    # Send email
+    return HttpResponse("Hello World")
