0f4d1a2c9fc6364f5cfdf31c335d21052ba10b19
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-03 16:41:00 +0500

    Generic Relationships

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,20 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+from store.models import Product
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TagItem(models.Model):
+    # What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type (product, video, article)
+    # ID
+    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
