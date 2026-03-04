ebc3b92b7712d3ddc14fc9f04274231dc6d2d684
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 13:44:28 +0500

    Managers and QuerySets

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Product
 
-# Create your views here.
-# request -> response
-# request handler
-# action
+
 def say_hello(request):
-    # Pull data from db
-    # Transform
-    # Send email
-    # return HttpResponse("Hello World")
+    queryset = Product.objects.all()
+    # queryset = Product.objects.count()
+
+    # for product in queryset:
+    #     print(product)
+
+    # list(queryset)
+
+    # queryset[0:5] # lazy
+
+    # queryset.filter().filter().order_by()
+
     return render(request, 'hello.html', {'name': 'Rovshen'})
