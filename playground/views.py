970653cae8d5079559c4d5ff590bf05a91880583
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 14:49:41 +0500

    Limiting Results

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,11 @@
-from django.db.models import F
 from django.shortcuts import render
 
 from store.models import Product
 
 
 def say_hello(request):
-    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/
-    queryset = Product.objects.order_by('title') # ASC
-    queryset = Product.objects.order_by('-title') # DESC
-    queryset = Product.objects.order_by('unit_price', '-title').reverse()
-
-    product = Product.objects.order_by('unit_price')[0]
-    product = Product.objects.earliest('unit_price')
+    # 0, 1, 2, 3, 4
+    queryset = Product.objects.all()[:5]
+    queryset = Product.objects.all()[5:10]
 
     return render(request, 'hello.html', {'name': 'Rovshen', 'products': list(queryset)})
