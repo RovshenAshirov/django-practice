2abbc1b311258b406a8f2a8b95a78ce640fde0c6
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 14:33:55 +0500

    Complex Lookups Using Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,15 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.shortcuts import render
 
 from store.models import Product
 
 
 def say_hello(request):
-    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/
-    # keyword=value
-    queryset  = Product.objects.filter(unit_price=20)
-    # queryset  = Product.objects.filter(unit_price>20) # Don't work
-    queryset  = Product.objects.filter(unit_price__gt=20) # greater
-    queryset  = Product.objects.filter(unit_price__lt=20) # little
-    queryset  = Product.objects.filter(unit_price__range=(20, 30))
-    queryset  = Product.objects.filter(collection__id__gt=1)
-    # queryset  = Product.objects.filter(collection__id__range=(1, 2, 3)) # error
-    queryset  = Product.objects.filter(title__contains='coffee') # like
-    queryset  = Product.objects.filter(title__icontains='coffee') # ilike
-    queryset  = Product.objects.filter(title__startswith='coffee') # like
-    queryset  = Product.objects.filter(updated_at__year=2021) # extract year
-    queryset  = Product.objects.filter(description__isnull=True) # is null
+    # Products: inventory < 10 AND price < 20
+    queryset  = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    queryset  = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+
+    # Products: inventory < 10 OR NOT price < 20
+    queryset  = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20)) # | - OR, & - AND, ~ - NOT
 
     return render(request, 'hello.html', {'name': 'Rovshen', 'products': list(queryset)})
