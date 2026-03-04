5ba10d2d0931da7b600947d2fb80d839a89125b2
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 14:46:47 +0500

    Sorting

--- playground/views.py
+++ playground/views.py
@@ -5,8 +5,12 @@ from store.models import Product
 
 
 def say_hello(request):
-    # Products: inventory = price
-    queryset = Product.objects.filter(inventory=F('unit_price'))
-    queryset = Product.objects.filter(inventory=F('collection__id'))
+    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/
+    queryset = Product.objects.order_by('title') # ASC
+    queryset = Product.objects.order_by('-title') # DESC
+    queryset = Product.objects.order_by('unit_price', '-title').reverse()
+
+    product = Product.objects.order_by('unit_price')[0]
+    product = Product.objects.earliest('unit_price')
 
     return render(request, 'hello.html', {'name': 'Rovshen', 'products': list(queryset)})
