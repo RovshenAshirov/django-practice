bf8b3ec3d2a693ffd76adbe42cbaf790f8946e20
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 21:54:18 +0500

    Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,18 @@
+from django.db.models import Count, Min
 from django.shortcuts import render
 
-from store.models import Product, Order
+from store.models import Product
 
 
 def say_hello(request):
-    # select_related (1)
-    # prefetch_related (n)
-
-    # queryset = Product.objects.all()
-    # queryset = Product.objects.select_related('collection')
-    # # queryset = Product.objects.select_related('collection__some_other_field')
-    # queryset = Product.objects.prefetch_related('promotions')
-    # queryset = Product.objects.select_related('collection').prefetch_related('promotions')
-
-    orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # Count, Min, Max, Avg, Sum
+    result = Product.objects.aggregate(Count('id'))
+    result = Product.objects.filter(collection_id=3).aggregate(
+        count=Count('id'),
+        min_price=Min('unit_price'),
+    )
 
     return render(request, 'hello.html', {
         'name': 'Rovshen',
-        # 'products': list(queryset),
-        'orders': list(orders),
+        'result': result
     })
