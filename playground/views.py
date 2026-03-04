c58ec2ad8f1d589591e7e013864bf3226a541576
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 14:22:01 +0500

    Filtering Objects

--- playground/views.py
+++ playground/views.py
@@ -5,17 +5,19 @@ from store.models import Product
 
 
 def say_hello(request):
-    # book = get_airplane()
-    try:
-        product = Product.objects.get(id=1) # or pk
-        # product = Product.objects.get(pk=0) # Error DoesNotExist
-    except ObjectDoesNotExist:
-        pass
+    # https://docs.djangoproject.com/en/4.2/ref/models/querysets/
+    # keyword=value
+    queryset  = Product.objects.filter(unit_price=20)
+    # queryset  = Product.objects.filter(unit_price>20) # Don't work
+    queryset  = Product.objects.filter(unit_price__gt=20) # greater
+    queryset  = Product.objects.filter(unit_price__lt=20) # little
+    queryset  = Product.objects.filter(unit_price__range=(20, 30))
+    queryset  = Product.objects.filter(collection__id__gt=1)
+    # queryset  = Product.objects.filter(collection__id__range=(1, 2, 3)) # error
+    queryset  = Product.objects.filter(title__contains='coffee') # like
+    queryset  = Product.objects.filter(title__icontains='coffee') # ilike
+    queryset  = Product.objects.filter(title__startswith='coffee') # like
+    queryset  = Product.objects.filter(updated_at__year=2021) # extract year
+    queryset  = Product.objects.filter(description__isnull=True) # is null
 
-    # None
-    product = Product.objects.filter(pk=0).first()
-
-    # True / False
-    exists = Product.objects.filter(pk=0).exists()
-
-    return render(request, 'hello.html', {'name': 'Rovshen'})
+    return render(request, 'hello.html', {'name': 'Rovshen', 'products': list(queryset)})
