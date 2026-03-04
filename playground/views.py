4f236c755cacbba4d4cb2469c9b1e792e8bc2c77
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-04 21:46:11 +0500

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Product, Order
 
 
 def say_hello(request):
-    queryset = Product.objects.only('id', 'title')
-    queryset = Product.objects.defer('description')
+    # select_related (1)
+    # prefetch_related (n)
 
-    return render(request, 'hello.html', {'name': 'Rovshen', 'products': list(queryset)})
+    # queryset = Product.objects.all()
+    # queryset = Product.objects.select_related('collection')
+    # # queryset = Product.objects.select_related('collection__some_other_field')
+    # queryset = Product.objects.prefetch_related('promotions')
+    # queryset = Product.objects.select_related('collection').prefetch_related('promotions')
+
+    orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+    return render(request, 'hello.html', {
+        'name': 'Rovshen',
+        # 'products': list(queryset),
+        'orders': list(orders),
+    })
