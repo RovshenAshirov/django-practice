b1cac8d9651d94cd18fd168ec240f4fe85f6aa36
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-05 11:28:11 +0500

    Creating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Product, Collection
 
 
 def say_hello(request):
-    # QuerySet lazy
-    # lazy loading
-    # It doesn’t send a new SQL query every time.
-    queryset = Product.objects.all()
-    queryset[0]
-    list(queryset)
-    list(queryset)
-    queryset[0]
+    # collection = Collection(title="Video Games")
+    collection = Collection()
+    collection.title = "Video Games"
+    collection.featured_product = Product(id=1)
+    # collection.featured_product_id = 1
+    collection.save()
+    print(collection.id)
+
+    # collection = Collection.objects.create(
+    #     title="Video Games",
+    #     featured_product=Product(id=1),
+    # )
 
     return render(request, 'hello.html', {
         'name': 'Rovshen'
