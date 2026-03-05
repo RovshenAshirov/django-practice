e12d195887daf1e57b357feb0e96b055f76234e5
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-05 23:28:48 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# The Ultimate Django Series: Part 2
+
+required: Python 3.12 version
+
+## Setting Up the Project
+
+```sql
+CREATE DATABASE storefront2
+```
+
+```bash
+pip install -r requirements.txt
+python manage.py migrate
+```
+
+Export seed.sql
+
+```bash
+python manage.py createsuperuser
+```
---
+++ store/admin.py
@@ -0,0 +1,97 @@
+from django.contrib import admin
+from django.core.checks import messages
+from django.db.models import QuerySet, Count
+from django.http import HttpRequest
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
+
+from store.models import Collection, Product, Customer, Order, OrderItem
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet) -> QuerySet:
+        if self.value() == '<10':
+            return queryset.filter(inventory__lte=10)
+        return queryset
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'updated_at', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+    search_fields = ['title']
+
+    def collection_title(self, obj):
+        return obj.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, obj):
+        if obj.inventory < 10:
+            return "Low"
+        return "OK"
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset: QuerySet):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f"{updated_count} products were successfully updated.",
+            messages.ERROR
+        )
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ('first_name', 'last_name')
+    list_per_page = 10
+    search_fields = ['first_name', 'last_name']
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, obj):
+        url = reverse('admin:store_product_changelist') + "?" + urlencode({'collection_id': obj.id})
+        return format_html('<a href="{}">{}</a>', url, obj.products_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
+
+class OrderItemInline(admin.StackedInline):
+    autocomplete_fields = ['product']
+    model = OrderItem
+    extra = 0
+    min_num = 1
+    max_num = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
