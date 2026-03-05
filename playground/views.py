20bb7f82679d92ad2c38bdc5ce3ca0c42cb179aa
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-05 13:29:11 +0500

    Deleting Objects

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
 
-from store.models import Product, Collection
+from store.models import Collection
 
 
 def say_hello(request):
-    # collection = Collection(pk=11)
-    # collection = Collection.objects.get(pk=11)
-    # # collection.title = "Games"
-    # collection.featured_product = None
-    # collection.save()
-    # print(collection.id)
+    collection = Collection(pk=11)
+    collection.delete()
 
-    # Collection.objects.update(featured_product=None) # for all
-    Collection.objects.filter(pk=11).update(featured_product=None)
+    Collection.objects.filter(pk=11).delete()
 
     return render(request, 'hello.html', {
         'name': 'Rovshen'
