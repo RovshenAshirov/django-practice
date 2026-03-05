621e014abfe74ae94a0962d117760ecd729ed94b
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-05 13:24:57 +0500

    Updating Objects

--- playground/views.py
+++ playground/views.py
@@ -4,18 +4,15 @@ from store.models import Product, Collection
 
 
 def say_hello(request):
-    # collection = Collection(title="Video Games")
-    collection = Collection()
-    collection.title = "Video Games"
-    collection.featured_product = Product(id=1)
-    # collection.featured_product_id = 1
-    collection.save()
-    print(collection.id)
+    # collection = Collection(pk=11)
+    # collection = Collection.objects.get(pk=11)
+    # # collection.title = "Games"
+    # collection.featured_product = None
+    # collection.save()
+    # print(collection.id)
 
-    # collection = Collection.objects.create(
-    #     title="Video Games",
-    #     featured_product=Product(id=1),
-    # )
+    # Collection.objects.update(featured_product=None) # for all
+    Collection.objects.filter(pk=11).update(featured_product=None)
 
     return render(request, 'hello.html', {
         'name': 'Rovshen'
