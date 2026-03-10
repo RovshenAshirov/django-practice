9ede791a4b4a60ee3b0ddaa2228455e791593dbb
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-10 16:45:46 +0500

    Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -5,24 +5,24 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    # https://www.django-rest-framework.org/api-guide/fields/
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
-    # collection = serializers.StringRelatedField()
-    # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail',
+    # )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'price_with_tax', 'collection']
+        # fields = '__all__'
 
     def calculate_tax(self, obj: Product) -> Decimal:
         return obj.unit_price * Decimal(1.1)
