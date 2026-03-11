4ad8799abcc6fc5355b41d28390f311144801443
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-11 16:36:35 +0500

    Class-based Views

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from store.views import product_list, product_detail, collection_detail, collection_list
+from store.views import collection_detail, collection_list, ProductList, ProductDetail
 
 urlpatterns = [
-    path('products/', product_list),
-    path('products/<int:pk>/', product_detail),
+    path('products/', ProductList.as_view()),
+    path('products/<int:pk>/', ProductDetail.as_view()),
     path('collections/', collection_list),
     path('collections/<int:pk>/', collection_detail, name='collection-detail'),
 ]
