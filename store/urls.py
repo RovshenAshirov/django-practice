eff31ac742db512bcd1fa91988fc805143b8ec71
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-11 17:09:09 +0500

    Customizing Generic Views

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from store.views import collection_detail, ProductListCreateView, ProductDetail, CollectionListCreateView
+from store.views import (
+    ProductListCreateView, ProductRetrieveUpdateDestroyView, CollectionListCreateView,
+    CollectionRetrieveUpdateDestroyView
+)
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view()),
-    path('products/<int:pk>/', ProductDetail.as_view()),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view()),
     path('collections/', CollectionListCreateView.as_view()),
-    path('collections/<int:pk>/', collection_detail, name='collection-detail'),
+    path('collections/<int:pk>/', CollectionRetrieveUpdateDestroyView.as_view(), name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,7 @@
 from django.db.models import Count
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404, ListCreateAPIView
+from rest_framework.generics import get_object_or_404, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from store.models import Product, Collection
 from store.serializers import ProductSerializer, CollectionSerializer
@@ -13,28 +11,14 @@ class ProductListCreateView(ListCreateAPIView):
     queryset = Product.objects.select_related('collection').order_by('-updated_at')
     serializer_class = ProductSerializer
 
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collection').order_by('-updated_at')
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
-
     def get_serializer_context(self):
         return {'request': self.request}
 
 
-class ProductDetail(APIView):
-    def get(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(instance=product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
 
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -45,25 +29,18 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class CollectionListCreateView(ListCreateAPIView):
     queryset = Collection.objects.annotate(products_count=Count('products'))
     serializer_class = CollectionSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-    collection = get_object_or_404(Collection.objects.annotate(
-        product_count=Count('products')
-    ), pk=pk)
-    if request.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CollectionSerializer(instance=collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
+class CollectionRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.annotate(product_count=Count('products'))
+    serializer_class = CollectionSerializer
+
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
             return Response({
                 'error': 'Collection cannot be deleted because it includes one or more products.'
