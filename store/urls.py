8fb176d007733aee4b472e19553472998c8017f7
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-11 18:16:12 +0500

    Filtering

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from store.views import ProductViewSet, CollectionViewSet, ReviewViewSet
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('collections', CollectionViewSet)
+router.register('products', ProductViewSet, basename='products')
+router.register('collections', CollectionViewSet, basename='collections')
 
 products_router = NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', ReviewViewSet, basename='reviews')
