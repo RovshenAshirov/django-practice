dd086bf1da4a0111cb9e7cacb87145fd44385f4b
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-03-11 16:57:42 +0500

    Generic Views

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from store.views import collection_detail, collection_list, ProductList, ProductDetail
+from store.views import collection_detail, ProductListCreateView, ProductDetail, CollectionListCreateView
 
 urlpatterns = [
-    path('products/', ProductList.as_view()),
+    path('products/', ProductListCreateView.as_view()),
     path('products/<int:pk>/', ProductDetail.as_view()),
-    path('collections/', collection_list),
+    path('collections/', CollectionListCreateView.as_view()),
     path('collections/<int:pk>/', collection_detail, name='collection-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
+from rest_framework.generics import get_object_or_404, ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,17 +9,18 @@ from store.models import Product, Collection
 from store.serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').order_by('-updated_at')
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductListCreateView(ListCreateAPIView):
+    queryset = Product.objects.select_related('collection').order_by('-updated_at')
+    serializer_class = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def get_queryset(self):
+    #     return Product.objects.select_related('collection').order_by('-updated_at')
+
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetail(APIView):
@@ -44,18 +45,9 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(products_count=Count('products'))
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionListCreateView(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('products'))
+    serializer_class = CollectionSerializer
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -78,5 +70,3 @@ def collection_detail(request, pk):
             }, status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# https://www.django-rest-framework.org/api-guide/generic-views/#mixins
